158f6cb2c94523b2168c42146f6017e91edbb601
SAULOPEZ <SAULOPEZ@SAULOPEZ-T480.mx.oracle.com>
2020-11-20 15:11:07 -0600

    # Add Transitions Table (UI only, basic validations)

---
+++ bootstrap-ui/js/readAutomata.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+
+    let template = ""
+
+    $("#iLanguage").change(function (value) {
+        createTT();
+    });
+
+    $("#iStates").change(function (value) {
+        createTT();
+    });
+
+    function createTT() {
+
+        // read inputs from UI
+        let inputLanguage = $("#iLanguage").val().trim();
+        let inputStates = $("#iStates").val().trim();
+
+
+        if (isValidInput(inputLanguage) && isValidInput(inputStates)) {
+
+            // split input
+            let language = inputLanguage.split(",");
+            let states = inputStates.split(",");
+            
+            // create table content
+            let header = createTableHeader(language);
+            let body = createTableBody(language, states);
+            
+            // read template
+            let template = $("#templateTT").html().trim();
+            
+            // replace markers 
+            template = template.replace("$t-head", header);
+            template = template.replace("$t-body", body);    
+
+            // draw table to UI
+            $("#tableContainer").html(template);
+
+        } else {
+            alert("verify yout inputs");
+        }
+    }
+
+    function isValidInput(input) {
+        return (input.length > 0 && input.charAt(0) != "," && input.charAt(input.length - 1) != ",")
+    }
+
+    function createTableHeader(language) {
+        let header = "<tr><th scope='col'>@</th>"
+        language.forEach(symbol => {
+            header += "<th scope='col'>" + symbol + "</th>";
+        });
+        return header + ("</tr>");
+    }
+
+    function createTableBody(language, states) {        
+        let body = "";
+        states.forEach(state => {
+            body += "<tr><th scope='row'>" + state + "</th>";
+            language.forEach(symbol => {
+                body += "<td>" + "*" + "</td>";
+            });
+            body += ("</tr>")   
+        });        
+        return body;
+    }
+
+});
